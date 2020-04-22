8cc61ac6f7e4100f1c4d4487dd022ddd35484be0
Luísa Parlow Siliprandi <lparlowsiliprandi@id.iit.edu>
2020-04-21 22:38:51 -0300

    Add sprite and Estela

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+//    Practice Week-10
+// const canvas = document.querySelector('.game');
+// const ctx = canvas.getContext('2d');
+
+// function tick() {
+//   console.log('tick');
+
+//   window.requestAnimationFrame(tick);
+// }
+
+// tick();
+
+const canvas = document.querySelector('.world');
+const ctx = canvas.getContext('2d');
+
+let walkingSprite;
+
+function init() {
+  walkingSprite = new Sprite({
+    x: 0,
+    y: 0,
+    frameCount: 3,
+    src: './assets/img/estela.png',
+  });
+
+  walkingSprite.animation = new AnimationFrame(8, () => draw());
+  walkingSprite.animation.start();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Position
+  walkingSprite.x = (canvas.width - walkingSprite.width) / 10;
+  walkingSprite.y = (canvas.height - walkingSprite.height) / 1.5;
+
+  walkingSprite.draw();
+  walkingSprite.nextFrame();
+}
+
+init();
