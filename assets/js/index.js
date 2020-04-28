f110945d5a576588f8a4b2c6f10e6d0f800296e9
Luísa Parlow Siliprandi <lparlowsiliprandi@id.iit.edu>
2020-04-27 23:44:59 -0300

    Add background + jump + state machine js files

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+//    Practice Week-10
+// const canvas = document.querySelector('.game');
+// const ctx = canvas.getContext('2d');
+
+// function tick() {
+//   console.log('tick');
+
+//   window.requestAnimationFrame(tick);
+// }
+
+// tick();
+
+const canvas = document.querySelector('.world');
+const ctx = canvas.getContext('2d');
+
+let walkingSprite;
+let paperAgenda1 = new paperAgenda(250, 160);
+
+function init() {
+  walkingSprite = new Sprite({
+    x: 0,
+    y: 0,
+    frameCount: 3,
+    src: './assets/img/estela.png',
+  });
+
+  walkingSprite.animation = new AnimationFrame(8, () => draw());
+  walkingSprite.animation.start();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Position
+  walkingSprite.x = (canvas.width - walkingSprite.width) / 10;
+  walkingSprite.y = (canvas.height - walkingSprite.height) / 0.15;
+
+  walkingSprite.draw();
+  walkingSprite.nextFrame();
+  paperAgenda1.draw();
+}
+
+init();
---
+++ assets/js/paper-agenda.js
@@ -0,0 +1,55 @@
+class paperAgenda {
+  image;
+  x;
+  y;
+  SpeedX = 1;
+  SpeedY = 1;
+  scale = 0.5;
+
+  constructor(theX, theY, speedX, speedY) {
+    console.log(theX, theY);
+
+    this.x = theX;
+    this.y = theY;
+
+    if (speedX !== undefined) {
+      this.SpeedX = speedX;
+    }
+    if (speedY !== undefined) {
+      this.SpeedY = speedY;
+    }
+
+    this.loadImages();
+  }
+
+  loadImages() {
+    this.image = new Image();
+
+    this.image.onload = () => {
+      this.draw();
+    };
+    this.image.src = './assets/img/paper-agenda.png';
+  }
+
+  draw() {
+    ctx.beginPath();
+    // drawImage(image, dx, dy, dWidth, dHeight);
+    ctx.drawImage(
+      this.image,
+      this.x,
+      this.y,
+      81 * this.scale,
+      111 * this.scale
+    );
+  }
+
+  move() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  tick() {
+    this.move();
+    this.draw();
+  }
+}
